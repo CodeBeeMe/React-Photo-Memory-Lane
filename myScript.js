94743fc4feb64cade07310302475d64a42b44711
Cătălin Anghel-Ursu <catalee.xen@gmail.com>
2018-08-10 15:27:27 +0300

    Update myScript.js
    Added new methods for the "browse albums" sidebar, that sets the chosen album as the "pictures" state of the main component. Now on picture-open the sidebar browse nav is closed(collapsed). Added a new "Intro" component for the "intro" animation. The next step is to add all the links for the photos in the 3 Photo Albums arrays and that will be the final important part of the project.

--- myScript.js
+++ myScript.js
@@ -23,31 +23,61 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
 
-//Title
-class Title extends React.Component {
+class Intro extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    const { openGallery, realignPictures, browseAlbums } = this.props;
-
+    const { startIntro } = this.props;
+    //console.log(AlbumTitle);
     return (
-      <div id="title" className="row justify-content-center">
-        <div id="browse" className="col- icons">
-          <i class="fas fa-images" onClick={browseAlbums} title="Browse Albums"/>
-        </div>
-        <div id="name" className="col- ">
-          <h1 onClick={openGallery}>Photo Memory Lane</h1>
-        </div>
-        <div id="realign" className="col- icons">
-          <i class="fas fa-align-justify" onClick={realignPictures} title="Realign Grid"/>
+      <div id="intro" className="">        
+        <div id="heart" className="">
+          <i class="far fa-heart" onClick={startIntro}></i>
         </div>
       </div>
     );
   }
 }
 
-const Footer = () => {
+//Title
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { pictures, openGallery, realignPictures, browseAlbums } = this.props;
+
+    const AlbumTitle = pictures[1][0].title;
+
+    //console.log(AlbumTitle);
+
+    return (
+      <div id="title" className="row justify-content-center enter">
+        <div id="browse" className="col- icons">
+          <i
+            class="fas fa-images"
+            onClick={browseAlbums}
+            title="Browse Albums"
+            />
+          <span>/{AlbumTitle}</span>
+        </div>
+        <div id="name" className="col- ">
+          <h1 onClick={openGallery}>Photo Memory Lane</h1>
+        </div>
+        <div id="realign" className="col- icons">
+          <i
+            class="fas fa-align-justify"
+            onClick={realignPictures}
+            title="Realign Grid"
+            />
+        </div>
+      </div>
+    );
+  }
+}
+
+const FOOTER = () => {
   return (
     <div id="footer" className="fixed-bottom text-center">
       <h6>
@@ -84,31 +114,49 @@ const Footer = () => {
 class Sidebar extends React.Component {
   constructor(props) {
     super(props);
-  
   }
-  
+
   render() {
+    const { setPhotoAlbum01, setPhotoAlbum02, setPhotoAlbum03 } = this.props;
 
     return (
       <div id="albums" class="">
-          <nav id="navbar" className="">
-            <header>
-              <div className="row justify-content-center">
-                <div className="col-">
-                  <h1><i class="far fa-images"></i>Photo</h1>
-                </div>
-                <div className="col-">
-                  <h5>Albums</h5>
-                </div>
+        <nav id="navbar" className="">
+          <header>
+            <div className="row justify-content-center">
+              <div className="col-">
+                <h1>
+                  <i class="far fa-images" />Photo
+                </h1>
               </div>
-            </header>
-            <ol id="section-nav" className="nav-list text-center">
-              <li id="0" className="active"><a className="nav-link" href="#Beginning">Beginning</a></li>
-              <li id="1"><a className="nav-link" href="#Wedding">Wedding</a></li>
-              <li id="2"><a className="nav-link" href="#Vacation">Vacation</a></li>
-            </ol>
-          </nav>          
-        </div>
+              <div className="col-">
+                <h5>Albums</h5>
+              </div>
+            </div>
+          </header>
+          <ol id="section-nav" className="nav-list text-center">
+            <li id="Beginning" className="active">
+              <a
+                className="nav-link"
+                href="#Beginning"
+                onClick={setPhotoAlbum01}
+                >
+                Beginning
+              </a>
+            </li>
+            <li id="Wedding">
+              <a className="nav-link" href="#Wedding" onClick={setPhotoAlbum02}>
+                Wedding
+              </a>
+            </li>
+            <li id="Travels">
+              <a className="nav-link" href="#Travels" onClick={setPhotoAlbum03}>
+                Travels
+              </a>
+            </li>
+          </ol>
+        </nav>
+      </div>
     );
   }
 }
@@ -116,7 +164,7 @@ class Sidebar extends React.Component {
 class Gallery extends React.Component {
   constructor(props) {
     super(props);
-    this.photoAlbum = [
+    this.photoAlbum01 = [
       [
         {
           id: "0",
@@ -158,47 +206,128 @@ class Gallery extends React.Component {
           id: "9",
           url: "https://cdn.stocksnap.io/img-thumbs/960w/YHYTODADM0.jpg"
         },
-        { id: "10", url: "" },
-        { id: "11", url: "" },
-        { id: "12", url: "" },
-        { id: "13", url: "" },
-        { id: "14", url: "" },
-        { id: "15", url: "" },
-        { id: "16", url: "" },
-        { id: "17", url: "" },
-        { id: "18", url: "" },
-        { id: "19", url: "" },
-        { id: "20", url: "" },
-        { id: "21", url: "" },
-        { id: "22", url: "" },
-        { id: "23", url: "" },
-        { id: "24", url: "" },
-        { id: "25", url: "" },
-        { id: "26", url: "" },
-        { id: "27", url: "" },
-        { id: "28", url: "" },
-        { id: "29", url: "" },
-        { id: "30", url: "" }
+        { id: "10", url: "" }
       ],
-      [{ title: "" }]
+      [{ title: "Beginning" }]
+    ];
+
+    this.photoAlbum02 = [
+      [
+        {
+          id: "0",
+          url: "https://cdn.stocksnap.io/img-thumbs/960w/FUFZOMY015.jpg"
+        },
+        {
+          id: "1",
+          url: "https://cdn.stocksnap.io/img-thumbs/960w/PN5JN0TAIU.jpg"
+        },
+        {
+          id: "2",
+          url: "https://cdn.stocksnap.io/img-thumbs/960w/LCBYOH4U9K.jpg"
+        },
+        {
+          id: "3",
+          url: "https://cdn.stocksnap.io/img-thumbs/960w/8ZSF5XPSEW.jpg"
+        },
+        {
+          id: "4",
+          url: "https://cdn.stocksnap.io/img-thumbs/960w/N7FUTW0RY5.jpg"
+        },
+        {
+          id: "5",
+          url: "https://cdn.stocksnap.io/img-thumbs/960w/VSC3718R56.jpg"
+        },
+        {
+          id: "6",
+          url: "https://cdn.stocksnap.io/img-thumbs/960w/D62F810162.jpg"
+        },
+        {
+          id: "7",
+          url: "https://cdn.stocksnap.io/img-thumbs/960w/C637BA80AA.jpg"
+        },
+        {
+          id: "8",
+          url: "https://cdn.stocksnap.io/img-thumbs/960w/CA7B4D0A9A.jpg"
+        },
+        {
+          id: "9",
+          url: "https://cdn.stocksnap.io/img-thumbs/960w/ZTXOFYBPSL.jpg"
+        },
+        { id: "10", url: "" }
+      ],
+      [{ title: "Wedding" }]
+    ];
+
+    this.photoAlbum03 = [
+      [
+        { id: "0", url: "" },
+        { id: "1", url: "" },
+        { id: "2", url: "" },
+        { id: "3", url: "" },
+        { id: "4", url: "" },
+        { id: "5", url: "" },
+        { id: "6", url: "" },
+        { id: "7", url: "" },
+        { id: "8", url: "" },
+        { id: "9", url: "" },
+        { id: "10", url: "" }
+      ],
+      [{ title: "Travels" }]
     ];
 
     this.state = {
-      pictures: this.photoAlbum,
+      pictures: this.photoAlbum01,
       active: false
     };
 
-    this.browseAlbums = this.browseAlbums.bind(this);
     this.realignPictures = this.realignPictures.bind(this);
+    this.setPhotoAlbum01 = this.setPhotoAlbum01.bind(this);
+    this.setPhotoAlbum02 = this.setPhotoAlbum02.bind(this);
+    this.setPhotoAlbum03 = this.setPhotoAlbum03.bind(this);    
+    this.browseAlbums = this.browseAlbums.bind(this);
     this.openPicture = this.openPicture.bind(this);
-    this.openGallery = this.openGallery.bind(this);    
+    this.openGallery = this.openGallery.bind(this);
+    this.startIntro = this.startIntro.bind(this);
+  }
+  
+  
+  startIntro() {
+    let uiHeart = $("#heart"),
+        uiIntro = $("#intro");
     
+    uiHeart
+      .addClass("heart-animation");
+    setTimeout(() => {
+      uiIntro.hide();
+      this.openGallery()
+    }, 1900);
+  }
+
+  setPhotoAlbum01() {
+    this.setState({
+      pictures: this.photoAlbum01
+    });
+    this.realignPictures();
+  }
+
+  setPhotoAlbum02() {
+    this.setState({
+      pictures: this.photoAlbum02
+    });
+    this.realignPictures();
+  }
+
+  setPhotoAlbum03() {
+    this.setState({
+      pictures: this.photoAlbum03
+    });
+    this.realignPictures();
   }
 
   browseAlbums() {
     let uiAlbums = $("#albums"),
         uiBrowse = $("#browse");
-    
+
     if (this.state.active === false) {
       uiBrowse.addClass("open");
       uiAlbums.show();
@@ -216,7 +345,7 @@ class Gallery extends React.Component {
       uiAlbums.addClass("albums-animation-out");
       setTimeout(() => {
         uiAlbums.hide();
-      }, 500) 
+      }, 500);
     }
   }
 
@@ -225,16 +354,19 @@ class Gallery extends React.Component {
         uiThumbLinks = $("#thumbs li"),
         uiTitleH1 = $("#title h1"),
         uiFooter = $("#footer"),
-        uiTitle = $("#title"),
-        uiIcons = $(".icons");
+        uiTitle = $("#title"),        
+        uiIcons = $(".icons"),
+        uiIntro = $("#intro");
     
     uiIcons.show();
+    uiTitle.show();
+    uiIntro.hide();
 
-    uiTitle
-      .removeClass("justify-content-center")
+    uiTitle      
+      .removeClass("enter")
       .addClass("justify-content-between")
       .css("margin-top", "0px")
-      .css("padding", "10px")
+      .css("padding", "12px 15px 10px 15px")
       .css("border-bottom", "3px solid #111")
       .addClass("title-grad")
       .addClass("fixed-top");
@@ -255,36 +387,48 @@ class Gallery extends React.Component {
     let uiThumbLinks = $("#thumbs li"),
         uiRealign = $("#realign");
     
+    //closes the browse sidebar if it's open
+    if(this.state.active === true) {
+      this.browseAlbums();
+    }
+    
     uiThumbLinks.click(function() {
       uiRealign.addClass("open");
       $(this)
         .addClass("active")
         .siblings()
         .removeClass("active");
+
+      setTimeout(() => {
+        $(this)
+          .get(0)
+          .scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+          inline: "nearest"
+        });
+      }, 100);
     });
-    //console.log(key);
-    //console.log(this.playKeys[key]);
   }
-  
+
   realignPictures() {
     let uiThumbLinks = $("#thumbs li"),
         uiRealign = $("#realign");
-    
+
     uiRealign.removeClass("open");
-    
-    uiRealign.click(function() {
-      uiThumbLinks.siblings().removeClass("active");
-    });
-    
+    uiThumbLinks.siblings().removeClass("active");
   }
 
   componentDidMount() {
+    let uiTitle = $("#title");
+    
+    uiTitle.hide();
+    
     window.focus();
-    this.realignPictures();
     this.openPicture();
-    
+
     let uiNavLinks = $(".nav-list li");
-    
+
     uiNavLinks.click(function() {
       $(this)
         .addClass("active")
@@ -295,11 +439,11 @@ class Gallery extends React.Component {
 
   render() {
     //Logs
-    //console.log(this.state.pads);
 
     const { pictures } = this.state;
+    console.log(pictures);
 
-    const snaps = pictures[0].map(el => (
+    const thumbs = pictures[0].map(el => (
       <li
         id={el.id}
         key={el.id}
@@ -311,24 +455,30 @@ class Gallery extends React.Component {
       </li>
     ));
 
-    const presetTitle = pictures[1][0].title;
-
     return (
       <div id="wrapper">
-        <Title
+        <Intro 
+          startIntro={this.startIntro}
+          />
+        <Navbar
+          pictures={this.state.pictures}
           openGallery={this.openGallery}
           realignPictures={this.realignPictures}
           browseAlbums={this.browseAlbums}
           />
-        <Sidebar />
+        <Sidebar
+          setPhotoAlbum01={this.setPhotoAlbum01}
+          setPhotoAlbum02={this.setPhotoAlbum02}
+          setPhotoAlbum03={this.setPhotoAlbum03}
+          />
         <div id="photo-gallery">
           <div className="thumbs-box">
             <ul id="thumbs" className="text-center">
-              {snaps}
+              {thumbs}
             </ul>
           </div>
         </div>
-        <Footer />
+        <FOOTER />
       </div>
     );
   }
